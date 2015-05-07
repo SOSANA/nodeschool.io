82e402e01c58508e2f04ae7d696bfdb80a6245b1
sosana <thesosana@gmail.com>
2015-05-07 01:55:30 +0000

    adding alternative solution to process.argv to coerce strings into numbers

--- learnyounode/babysteps/program.js
+++ learnyounode/babysteps/program.js
@@ -1,18 +1,22 @@
+/*
+ * process.argv is an array containing the command line arguments. The first element will 
+ * be 'node', the second element will be the name of the JavaScript file. The next elements 
+ * will be any additional command line arguments.
+ * 
+ * Be aware that all elements of process.argv are strings and you may need to coerce them into 
+ * numbers. You can do this by prefixing the property with + or passing it to Number(). e.g. 
+ * +process.argv[2] or Number(process.argv[2]).  
+*/
+
 var result = 0;
 
-for (var i = 2; i < processs.argv.length; i++) {
-    result += Number(procss.argv[i]);
+for (var i = 2; i < process.argv.length; i++) {
+    result += Number(process.argv[i]);
+    // or you could right it like this
+    //result += +process.argv[i];
 }
 
 console.log(result);
 
 
 
-/*
- * var result = 0;
-
-for (var i = 2; i < process.argv.length; i++){
-result += Number(process.argv[i])
-}
-
-console.log(result)
