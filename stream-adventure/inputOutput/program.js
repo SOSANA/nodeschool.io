d700519760e4ad6d6d6df530c2fd9e778aab13dd
sosana <thesosana@gmail.com>
2015-05-17 22:00:48 +0000

    created input/out

--- stream-adventure/inputOutput/program.js
+++ stream-adventure/inputOutput/program.js
@@ -2,3 +2,13 @@
  * add notes here
  * 
 */
+
+var fs = require('fs');
+
+
+var processData = function() {
+    // taking input and putting output
+    process.stdin.pipe(process.stdout);    
+}
+
+processData();
