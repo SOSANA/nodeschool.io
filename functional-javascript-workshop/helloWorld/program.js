66e45ba2f86b01a6ae3ee80055f9ad3fc8fa7340
sosana <thesosana@gmail.com>
2015-05-19 01:16:12 +0000

    Wrote a function that takes an input string and returns it uppercased.

--- functional-javascript-workshop/helloWorld/program.js
+++ functional-javascript-workshop/helloWorld/program.js
@@ -1,4 +1,26 @@
 /*
- * add notes here
+ * Task
+ * Write a function that takes an input string and returns it uppercased.
+ * 
+ * Arguments   
+ * input: a String of random words (lorem ipsum).
  * 
 */
+
+function upperCaser(input) {                                                                                               
+      // SOLUTION GOES HERE 
+      var inputUpperCased = input.toUpperCase();
+      return inputUpperCased;
+    } 
+
+module.exports = upperCaser
+
+// Here's the official solution in case you want to compare notes:
+/*
+module.exports = function(input) {
+    return input.toUpperCase()
+}
+*/
+    
+    
+ 
